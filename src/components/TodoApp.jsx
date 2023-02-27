447a9dd08b1f0bdae67a7e9baff7134549e10527
Caren-Koroeny <carensiya5@gmail.com>
2023-02-27 17:00:59 +0300

    add external css

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,12 +1,16 @@
 import Header from "./Header";
 import TodosLogic from "./TodosLogic";
 
+
 const TodoApp = () => {
   return (
-    <>
+    <div className="wrapper">
+    <div className="todos">
+    
     <Header />
     <TodosLogic />
-    </>
+    </div>
+    </div>
   )
 }
 export default TodoApp;
